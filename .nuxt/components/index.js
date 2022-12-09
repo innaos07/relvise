4ec18aba680092a2e975e93879583115dbedc4e1
innaos07 <samosudova07@gmail.com>
2022-12-10 00:50:55 +0100

    build

--- .nuxt/components/index.js
+++ .nuxt/components/index.js
@@ -2,8 +2,6 @@ export const FooterComponent = () => import('../../components/FooterComponent.vu
 export const HeaderComponent = () => import('../../components/HeaderComponent.vue' /* webpackChunkName: "components/header-component" */).then(c => wrapFunctional(c.default || c))
 export const MainComponent = () => import('../../components/MainComponent.vue' /* webpackChunkName: "components/main-component" */).then(c => wrapFunctional(c.default || c))
 export const UIButtonLink = () => import('../../components/UI/ButtonLink.vue' /* webpackChunkName: "components/u-i-button-link" */).then(c => wrapFunctional(c.default || c))
-export const FooterBlock = () => import('../../components/footer/FooterBlock.vue' /* webpackChunkName: "components/footer-block" */).then(c => wrapFunctional(c.default || c))
-export const FooterHeaderFooter = () => import('../../components/footer/HeaderFooter.vue' /* webpackChunkName: "components/footer-header-footer" */).then(c => wrapFunctional(c.default || c))
 export const MainAboutBlock = () => import('../../components/main/AboutBlock.vue' /* webpackChunkName: "components/main-about-block" */).then(c => wrapFunctional(c.default || c))
 export const MainAdvantagesBlock = () => import('../../components/main/AdvantagesBlock.vue' /* webpackChunkName: "components/main-advantages-block" */).then(c => wrapFunctional(c.default || c))
 export const MainClientsBlock = () => import('../../components/main/ClientsBlock.vue' /* webpackChunkName: "components/main-clients-block" */).then(c => wrapFunctional(c.default || c))
@@ -12,6 +10,8 @@ export const MainPracticeBlock = () => import('../../components/main/PracticeBlo
 export const MainPromoBlock = () => import('../../components/main/PromoBlock.vue' /* webpackChunkName: "components/main-promo-block" */).then(c => wrapFunctional(c.default || c))
 export const MainServicesBlock = () => import('../../components/main/ServicesBlock.vue' /* webpackChunkName: "components/main-services-block" */).then(c => wrapFunctional(c.default || c))
 export const MainSubscribeBlock = () => import('../../components/main/SubscribeBlock.vue' /* webpackChunkName: "components/main-subscribe-block" */).then(c => wrapFunctional(c.default || c))
+export const FooterBlock = () => import('../../components/footer/FooterBlock.vue' /* webpackChunkName: "components/footer-block" */).then(c => wrapFunctional(c.default || c))
+export const FooterHeaderFooter = () => import('../../components/footer/HeaderFooter.vue' /* webpackChunkName: "components/footer-header-footer" */).then(c => wrapFunctional(c.default || c))
 
 // nuxt/nuxt.js#8607
 function wrapFunctional(options) {
